f51ee7934066f35bea72caae74bb04f5b7e31d7c
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-05 02:52:11 +0900

    feat: color 추가

---
+++ src/libs/Colors.ts
@@ -0,0 +1,30 @@
+export const colors = {
+  main1: '#8457F1',
+  main2: '#F2EEFD',
+  main3: '#332A53',
+  l_sub_1: '#D7CEF7',
+  l_sub_2: '#F2EEFD',
+  d_sub_1: '#362856',
+  d_sub_2: '#1F1B2A',
+  d_sub_3: '#332A53',
+
+  red: '#FF3F57',
+  green: '#19CFA2',
+  blue: '#3478F6',
+  white: '#FFFFFF',
+
+  grey_1: '#F6F8FA',
+  grey_2: '#E8EBF0',
+  grey_3: '#E0E2EE',
+  grey_4: '#C4C5D4',
+  grey_5: '#A5A9BD',
+  grey_6: '#8D91A5',
+  grey_7: '#595F72',
+  grey_8: '#353C49',
+  grey_9: '#1A1E27',
+
+  black: '#141414',
+
+  l_background: '#FFFFFF',
+  d_background: '#1A1E27',
+}
