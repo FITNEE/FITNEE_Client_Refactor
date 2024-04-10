efac8d4c0479c27617ce4cff930deb2b6f934ba6
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-11 02:44:07 +0900

    feat: 운동사전 검색 키워드 컴포넌트 개발

--- src/pages/Dictionary/Dictionary.tsx
+++ src/pages/Dictionary/Dictionary.tsx
@@ -1,8 +1,10 @@
 import ScreenLayout from '@/components/ScreenLayout'
+import KeywordList from '@/components/dictionary/KeywordList'
 import SearchBar from '@/components/dictionary/SearchBar'
 import { colors } from '@/libs/Colors'
 import React, { useState } from 'react'
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native'
+import styled from 'styled-components/native'
 
 export default function Dictionary() {
   const [searchText, setSearchText] = useState<string>('')
@@ -10,6 +12,27 @@ export default function Dictionary() {
   return (
     <ScreenLayout>
       <SearchBar text={searchText} setText={setSearchText} />
+      {searchText.length === 0 && (
+        <>
+          <Divider />
+          <KeywordList
+            title="최근 검색 키워드"
+            keywords={['데드리프트', '사이드 레터럴 레이즈']}
+            style={{ marginTop: 40 }}
+          />
+          <KeywordList
+            title="인기 키워드"
+            keywords={['데드리프트', '사이드 레터럴 레이즈']}
+            style={{ marginTop: 56 }}
+          />
+        </>
+      )}
     </ScreenLayout>
   )
 }
+
+const Divider = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: ${colors.grey2};
+`
