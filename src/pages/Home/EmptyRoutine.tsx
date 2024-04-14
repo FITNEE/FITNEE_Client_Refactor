f761aa0eede92bcca8d3ef4aa4cc70b72d0a2408
Choi Seho <kindseho@naver.com>
2024-04-14 11:44:46 +0900

    chore: Logo.svg 추가

---
+++ src/pages/Home/EmptyRoutine.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function EmptyRoutine() {
+  return (
+    <View>
+      <Text>EmptyRoutine</Text>
+    </View>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,28 @@
 import ScreenLayout from "@/components/home/ScreenLayout";
-import { View, Text } from "react-native";
+import Logo from "@/assets/images/SVGs/icon/Logo.svg";
+import { useAtom } from "jotai";
+import { isDarkModeAtom } from "@/states/GlobalStates";
+import { colors } from "@/libs/Colors";
+import styled from "styled-components/native";
+import Routine from "./Routine";
+import EmptyRoutine from "./EmptyRoutine";
 
 export default function Home() {
+  const [isDark] = useAtom(isDarkModeAtom);
   return (
     <ScreenLayout>
-      <View>
-        <Text>home</Text>
-      </View>
+      <LogoWrapper>
+        <Logo
+          width={88}
+          height={28}
+          color={isDark ? colors.white : colors.black}
+        />
+      </LogoWrapper>
+      {true ? <Routine /> : <EmptyRoutine />}
     </ScreenLayout>
   );
 }
+
+const LogoWrapper = styled.View`
+  padding: 10px 24px;
+`;
