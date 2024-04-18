86012cd9ac37c08f9e2ff0e5494ecc2bd981a1ae
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-18 18:36:02 +0900

    feat: 운동사전 세부정보 보여주는 화면 구성

---
+++ src/components/dictionary/BottomSheet.tsx
@@ -0,0 +1,134 @@
+import styled from 'styled-components/native'
+import BottomSheet, {
+  BottomSheetScrollView,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet'
+import { useMemo } from 'react'
+import { colors } from '@/libs/Colors'
+import AddIcon from '@/assets/images/SVGs/icon/Plus.svg'
+import { screenWidth } from '@/libs/Dimensions'
+import Process from './Process'
+import Caution from './Caution'
+
+const exerciseInfoList = [
+  {
+    title: '안장 높낮이 조절',
+    description:
+      '안장의 높이를 삼두 중앙보다 약간 위쪽과 같도록 맞춘 후 손잡이를 잡아주세요.',
+  },
+  {
+    title: '안장 높낮이 조절',
+    description:
+      '안장의 높이를 삼두 중앙보다 약간 위쪽과 같도록 맞춘 후 손잡이를 잡아주세요.',
+  },
+  {
+    title: '안장 높낮이 조절',
+    description:
+      '안장의 높이를 삼두 중앙보다 약간 위쪽과 같도록 맞춘 후 손잡이를 잡아주세요.',
+  },
+] //dummy
+const cautionList = [
+  '적절한 무게로 승모근에 무리가 가지 않도록 하세요.',
+  '적절한 무게로 승모근에 무리가 가지 않도록 하세요.',
+  '적절한 무게로 승모근에 무리가 가지 않도록 하세요.',
+] //dummy
+
+export default function DictionaryBottomSheet() {
+  const snapPoints = useMemo(() => ['45%', '92%'], [])
+
+  return (
+    <BottomSheet snapPoints={snapPoints} index={0}>
+      <BottomSheetView>
+        <TopContainer>
+          <PartText>어깨 | 측면 삼각근 | 머신</PartText>
+          <NameText>사이드 레터럴 레이즈 머신</NameText>
+          <AddButton>
+            <AddIcon width={18} height={18} color={colors.main1} />
+          </AddButton>
+        </TopContainer>
+        <TabContainer>
+          <Tab>
+            <TabText>운동방법</TabText>
+          </Tab>
+        </TabContainer>
+      </BottomSheetView>
+      <Content showsVerticalScrollIndicator={false}>
+        <ProcessContainer>
+          {exerciseInfoList.map((exerciseInfo, index) => (
+            <Process
+              index={index + 1}
+              title={exerciseInfo.title}
+              description={exerciseInfo.description}
+              key={index}
+            />
+          ))}
+        </ProcessContainer>
+        <Caution description={cautionList} />
+      </Content>
+    </BottomSheet>
+  )
+}
+
+const TopContainer = styled.View`
+  padding: 24px;
+  gap: 4px;
+  width: 100%;
+  position: relative;
+`
+const PartText = styled.Text`
+  color: ${colors.main1};
+  font-family: 'Regular';
+  font-size: 13px;
+`
+const NameText = styled.Text`
+  color: ${colors.black};
+  font-family: 'SemiBold';
+  font-size: 24px;
+`
+const AddButton = styled.TouchableOpacity`
+  background-color: ${colors.main2};
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 20px;
+  right: 24px;
+`
+
+const TabContainer = styled.View`
+  border-bottom-width: 1px;
+  border-bottom-color: ${colors.grey2};
+  width: 100%;
+  height: 44px;
+  margin-top: 8px;
+  position: relative;
+`
+const Tab = styled.View`
+  width: ${`${screenWidth / 2 - 32}px`};
+  padding: 11px 0;
+  border-bottom-width: 1px;
+  border-bottom-color: ${colors.main1};
+  position: absolute;
+  bottom: -1px;
+  left: 24px;
+  align-items: center;
+`
+const TabText = styled.Text`
+  font-size: 15px;
+  font-family: 'SemiBold';
+  color: ${colors.black};
+`
+const Content = styled(BottomSheetScrollView)`
+  padding: 16px 24px;
+  width: 100%;
+`
+const ProcessContainer = styled.View`
+  width: 100%;
+  gap: 8px;
+  margin-bottom: 36px;
+`
+const CautionContainer = styled.View`
+  gap: 2px;
+`
--- src/navigators/DictionaryNav.tsx
+++ src/navigators/DictionaryNav.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Home from '@/pages/home/Home'
 import Dictionary from '@/pages/dictionary/Dictionary'
 import Search from '@/pages/dictionary/Search'
+import Info from '@/pages/dictionary/Info'
 
 const Stack = createStackNavigator()
 
@@ -16,6 +17,7 @@ export default function DictionaryNav() {
     >
       <Stack.Screen name="DictionaryMain" component={Dictionary} />
       <Stack.Screen name="DictionarySearch" component={Search} />
+      <Stack.Screen name="DictionaryInfo" component={Info} />
     </Stack.Navigator>
   )
 }
