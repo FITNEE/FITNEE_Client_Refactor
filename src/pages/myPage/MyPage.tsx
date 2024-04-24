e23980de089d350f0bc9b5f518fb20c52fe850e5
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-25 02:58:43 +0900

    chore: 폴더 대소문자 구분 가능하게 config 수정

---
+++ src/pages/myPage/MyPage.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function MyPage() {
+  return (
+    <View>
+      <Text>mypage</Text>
+    </View>
+  )
+}
---
+++ src/pages/myRoutine/MyRoutine.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function MyRoutine() {
+  return (
+    <View>
+      <Text>MyRoutine</Text>
+    </View>
+  )
+}
