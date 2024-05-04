08f4a072923656ba042db3b5402d9b32fa5ccfcb
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-05-04 21:52:00 +0900

    feat: 운동사전 gif 추가

--- src/pages/dictionary/Info.tsx
+++ src/pages/dictionary/Info.tsx
@@ -4,6 +4,7 @@ import BackIcon from '@/assets/images/SVGs/icon/LeftArrow.svg'
 import { TouchableOpacity } from 'react-native'
 import { colors } from '@/libs/Colors'
 import DictionaryBottomSheet from '@/components/dictionary/BottomSheet'
+import { getGIF } from '@/utils/getImage'
 
 export default function DictionaryInfo() {
   return (
@@ -13,11 +14,23 @@ export default function DictionaryInfo() {
           <BackIcon width={24} height={24} color={colors.black} />
         </TouchableOpacity>
       </TopContainer>
+      <GifWrapper>
+        <Gif source={getGIF[1]} />
+      </GifWrapper>
       <DictionaryBottomSheet />
     </ScreenLayout>
   )
 }
 
+const GifWrapper = styled.View`
+  align-self: center;
+  width: 75%;
+`
+const Gif = styled.Image`
+  aspect-ratio: 1;
+  width: 100%;
+  height: undefined;
+`
 const TopContainer = styled.View`
   flex-direction: row;
   justify-content: space-between;
