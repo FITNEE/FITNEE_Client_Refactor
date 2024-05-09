4122d639fcb8c5bd7365717446e77417c4db9ca7
HIHJH <2170095@ewhain.net>
2024-05-09 23:43:24 +0900

    feat: 로그인 상태 관리 atom 추가

--- src/states/GlobalStates.ts
+++ src/states/GlobalStates.ts
@@ -2,3 +2,4 @@ import { atom } from 'jotai'
 
 export const isDarkModeAtom = atom<boolean>(false)
 export const isTabVisibleAtom = atom<boolean>(true)
+export const isLoggedInAtom = atom<boolean>(false)
